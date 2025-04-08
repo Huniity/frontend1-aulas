9bcd38937bdf883ae4c7f06a839ed52b36d7b467
Huniity <adrien.dejonc@gmail.com>
2025-04-08 04:42:20 +0100

    CHORE: MAIN Script revamp

--- scripts/posts.js
+++ scripts/posts.js
@@ -23,9 +23,9 @@ document.addEventListener("DOMContentLoaded", async function () {
           ${entry.match}<br><br>
           ${entry.result}<br><br>
           ${entry.match_result}<br>
-          <input type="text" style="padding: 8px 0px 8px 0px; width: 100%; border: 1px solid #ccc; border-radius: 5px; background-color: #f1f1f1; margin-top: 20px; margin-bottom: 2.5px" placeholder="Write your comment">
+          <input type="text" style="padding: 8px 0px 8px 0px; width: 100%; border: 1px solid #ccc; border-radius: 5px; background-color: #f1f1f1; margin-top: 20px; margin-bottom: 2.5px; box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;" placeholder="Write your comment">
           <button type="submit" style="padding: 10px 20px; border: none; cursor: pointer; border-radius: 5px; color: #1e1e1e; background-color: #f1c40f">Post!</button>
-        `;
+          `;
         matchList.appendChild(div);
       });
     }
--- scripts/redirect.js
+++
@@ -1,9 +0,0 @@
-function redirect () {
-    setTimeout(myURL, 5000);
-    var result = document.getElementById("result");
-    result.innerHTML = "<b> The page will redirect after delay of 5 seconds";
- }
-
- function myURL() {
-    document.location.href = 'http://http://127.0.0.1:5500/posts.html';
- }
