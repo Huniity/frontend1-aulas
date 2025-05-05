083edff8bad0a7f0d2ea7335450ba95f3bd3b1af
Huniity <adrien_dejonc@eticlagarve.com>
2025-05-05 22:07:21 +0100

    FEAT: TODO APP

---
+++ project/lib/mock_api.js
@@ -0,0 +1,39 @@
+const apiURL = "https://67f56836913986b16fa476aa.mockapi.io/api/";
+
+export const getTodo = async () => {
+    const response = await fetch(apiURL + "todos");
+    const data = await response.json();
+    return data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+};
+
+export const createTodo = async (postData) => {
+    const response = await fetch(apiURL + "todos", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+    });
+    return await response.json();
+};
+
+export const editTodo = async (id, updatedData) => {
+    const response = await fetch(apiURL + `todos/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedData)
+    });
+    return await response.json();
+};
+
+export const deleteTodo = async (id) => {
+    const response = await fetch(apiURL + `todos/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    return await response.json();
+};
